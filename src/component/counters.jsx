3d46892e36d716d1377deadd777dd24b8e10fa31
tusrr <tushar.raj2000@gmail.com>
2023-05-02 23:07:40 +0530

    Changes to be committed: 	modified:   src/component/counters.jsx

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -11,6 +11,16 @@ class Counters extends Component {
         ]
      } 
 
+     handleReset = () =>{
+        const counters = this.state.counters.map(c=>{
+            c.value = 0;
+            return c;  
+        });
+// changed state..but not observed on dom--bcoz-- we are updating the value property 
+// based on what we get from our props -- this is executed 
+// only once when an instance of counter component is created
+     };
+
      handleDelete= (counterId)=>{
         // console.log('Event Handler called',counterId);
         const counters = this.state.counters.filter(c=>c.id !== counterId);
@@ -21,12 +31,18 @@ class Counters extends Component {
 
        
         return (<div> 
+
+            <button onClick={this.handleReset}
+            type="button" class="btn btn-primary  m-3 ">Reset</button>
+
             {this.state.counters.map(counter =>(
             <Counter 
             key={counter.id} //used internally by react 
             onDelete={this.handleDelete}
-            value={counter.value} 
-            id={counter.id}/>))}
+            // value={counter.value} 
+            // id={counter.id}
+            counter={counter}
+            />))}
                 {/* <h4>counter#{counter.id}</h4>
                 </Counter>)} */}
         </div>); 
